1cd0d49b35290083c615a5c27efc8da35c531b45
andrewyung <andrewyung@uvic.ca>
2018-03-03 01:47:22 -0800

    Added function to create light

--- OpenGLModelTriangularizing/ModelLoader.cpp
+++ OpenGLModelTriangularizing/ModelLoader.cpp
@@ -124,6 +124,70 @@ Model* ModelLoader::createPrimitive(PrimitiveTypes type)
 	return model;
 }
 
+Light * ModelLoader::createLight()
+{
+	std::vector<Vertex> vertexData;
+	std::vector<int> indexData;
+
+	//use cube as light model
+	vertexData = { Vertex(0.5f, 0.5f, 0.5f,	//front top right
+					0, 0, 1, 1,
+					0, 0,
+					1, 1, 1),
+					Vertex(-0.5f, 0.5f, 0.5f,	//front top left
+						0, 0, 1, 1,
+						0, 0,
+						-1, 1, 1),
+					Vertex(-0.5f, -0.5f, 0.5f,	//front bottom left
+						0, 0, 1, 1,
+						0, 0,
+						-1, -1, 1),
+					Vertex(0.5f, -0.5f, 0.5f,	//front bottom right
+						0, 0, 1, 1,
+						0, 0,
+						1, -1, 1),
+					Vertex(0.5f, 0.5f, -0.5f,	//back top right
+						0, 0, 1, 1,
+						0, 0,
+						1, 1, -1),
+					Vertex(-0.5f, 0.5f, -0.5f,	//back top left
+						0, 0, 1, 1,
+						0, 0,
+						-1, 1, -1),
+					Vertex(-0.5f, -0.5f, -0.5f,//back bottom left
+						0, 0, 1, 1,
+						0, 0,
+						-1, -1, -1),
+					Vertex(0.5f, -0.5f, -0.5f,	//back bottom right
+						0, 0, 1, 1,
+						0, 0,
+						1, -1, -1),
+	};
+
+	indexData = { 0, 1, 2,	//front
+		0, 2, 3,
+
+		4, 0, 3,	//right
+		4, 3, 7,
+
+		1, 5, 6,	//left
+		1, 6, 2,
+
+		4, 5, 1,	//top
+		4, 1, 0,
+
+		3, 2, 6,	//bottom
+		3, 6, 7,
+
+		7, 6, 5,
+		7, 5, 4
+	};
+
+	Light *light = new Light(vertexData, indexData);
+
+	return light;
+}
+
 Model * ModelLoader::loadModel(std::string objPath)
 {
 	objPath = "Models/" + objPath;
