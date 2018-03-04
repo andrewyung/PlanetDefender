c24898470bd0c9b3f1286707f6a0f5569e45613a
andrewyung <andrewyung@uvic.ca>
2018-03-03 22:33:30 -0800

    Updated Diffuse fragment shader
    For use of up to 9 lights

--- OpenGLModelTriangularizing/shaders/DiffuseFrag.fs
+++ OpenGLModelTriangularizing/shaders/DiffuseFrag.fs
@@ -10,11 +10,12 @@ uniform vec3 lightColor[MAX_LIGHTS];
 
 void main()
 {
-	float attenuation = 1 / length(v2fWorldPos - lightPos[0].xyz);
-	float dotProd = max(dot(normalize(v2fNormal), normalize(v2fWorldPos - lightPos[0].xyz)), 0.0f);
-	fragColor = lightPos[0].w * dotProd * attenuation * vec4(lightColor[0].xyz, 1);
-	
-	attenuation = 1 / length(v2fWorldPos - lightPos[1].xyz);
-	dotProd = max(dot(normalize(v2fNormal), normalize(v2fWorldPos - lightPos[1].xyz)), 0.0f);
-	fragColor = lightPos[1].w * dotProd * attenuation * vec4(lightColor[1].xyz, 1) + vec4(0.1f, 0.1f, 0.1f, 1) + fragColor;
+	for (int i = 0; i < MAX_LIGHTS; i++)
+	{
+		float attenuation = 1 / length(v2fWorldPos - lightPos[i].xyz);
+		float dotProd = max(dot(normalize(v2fNormal), normalize(v2fWorldPos - lightPos[i].xyz)), 0.0f);
+		fragColor = (lightPos[i].w * dotProd * attenuation * vec4(lightColor[i].xyz, 1)) + fragColor;
+	}
+
+	fragColor = vec4(0.1f, 0.1f, 0.1f, 1) + fragColor;
 }
