f9c4d3274e372f9a3c3c58c9b9a896e7ae3d255a
andrewyung <andrewyung@uvic.ca>
2018-03-04 00:19:16 -0800

    Added strength of light in diffuse shader
    strength of light is set as w component of light position

--- OpenGLModelTriangularizing/shaders/DiffuseFrag.fs
+++ OpenGLModelTriangularizing/shaders/DiffuseFrag.fs
@@ -10,12 +10,14 @@ uniform vec3 lightColor[MAX_LIGHTS];
 
 void main()
 {
+	vec4 color = vec4(0, 0, 0, 1);
+	float attenuation;
 	for (int i = 0; i < MAX_LIGHTS; i++)
 	{
-		float attenuation = 1 / length(v2fWorldPos - lightPos[i].xyz);
+		attenuation = 1.0f / (length(v2fWorldPos - lightPos[i].xyz));
 		float dotProd = max(dot(normalize(v2fNormal), normalize(v2fWorldPos - lightPos[i].xyz)), 0.0f);
-		fragColor = (lightPos[i].w * dotProd * attenuation * vec4(lightColor[i].xyz, 1)) + fragColor;
+		color += lightPos[i].w * (dotProd * attenuation * vec4(lightColor[i].xyz, 1));
 	}
 
-	fragColor = vec4(0.1f, 0.1f, 0.1f, 1) + fragColor;
+	fragColor = vec4(0.1f, 0.1f, 0.1f, 1) + color;
 }
