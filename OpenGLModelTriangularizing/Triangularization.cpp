56952f300f65942e297bce0c53c5afa2c033b0dc
andrewyung <andrewyung@uvic.ca>
2019-12-15 03:01:50 -0500

    Defined process for determining is convex vertex

--- OpenGLModelTriangularizing/Triangularization.cpp
+++ OpenGLModelTriangularizing/Triangularization.cpp
@@ -1,7 +1,21 @@
-#include "Triangularization.h"
+﻿#include "Triangularization.h"
 
-bool isConvex(Vertex v1, Vertex v2, Vertex v3)
+using namespace std;
+
+bool isConvexVertex(Vertex v1, Vertex v2, Vertex v3)
 {
+	// v1 - v2
+	vector<float> a { v1.x, v1.y, v1.z };
+	// v3 - v2
+	vector<float> b { v1.x, v1.y, v1.z };
+
+	//inner_product()
+	// find cross product of a and b
+	// det[abu]=a×b⋅u where u is cross prod of a and b
+	// if result is positive then right-handed, if negative then left-handed
+
+	// check angle (cos0)
+	// if angle is < 180 and is left-handed then convex
 
 }
 
--- OpenGLModelTriangularizing/Triangularization.h
+++ OpenGLModelTriangularizing/Triangularization.h
@@ -2,11 +2,14 @@
 #include "Vertex.h"
 #include "Model.h"
 
+#include <vector>
+#include <math.h>
+
 class Triangularization
 {
 public:
 	// Checks if v2 is a concave vertex. v1 and v3 are it's adjacent vertices and v1, v2 and v3 is assumed to be counter-clockwise
-	bool isConcaveVertex(Vertex v1, Vertex v2, Vertex v3);
+	bool isConvexVertex(Vertex v1, Vertex v2, Vertex v3);
 
 	// http://www-cgrl.cs.mcgill.ca/~godfried/teaching/cg-projects/97/Ian/algorithm1.html
 	// Triangularizes 2D polygons
