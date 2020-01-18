aa22e81766bc74e18f951c286af409805d0d0a88
andrewyung <andrewyung@uvic.ca>
2020-01-17 22:01:23 -0500

    Defined EllipsoidCollider.h

--- PlanetDefender/EllipsoidCollider.h
+++ PlanetDefender/EllipsoidCollider.h
@@ -6,6 +6,19 @@
 
 struct EllipsoidCollider : ColliderProperties
 {
-	glm::vec3 center;
-	glm::vec3 dimensions;
+public:
+	void setOrientation(glm::vec3 center, glm::vec3 dimensions);
+
+	glm::vec3 getDimensions()
+	{
+		return _dimensions;
+	}
+
+private:
+	glm::vec3 _center;
+	glm::vec3 _dimensions;
+
+	glm::vec3 _foci1;
+	glm::vec3 _foci2;
+	float _fociDistanceSum;
 };
