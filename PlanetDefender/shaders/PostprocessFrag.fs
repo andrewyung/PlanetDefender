e3b93fd5a6e620c8d7a63410786536709ddb4936
andrewyung <andrewyung@uvic.ca>
2020-01-22 23:05:06 -0500

    Post process shader (for screen quad)

---
+++ PlanetDefender/shaders/PostprocessFrag.fs
@@ -0,0 +1,12 @@
+#version 420 core
+in vec2 uvCoord;
+out vec4 fragColor;
+
+layout(binding = 0) uniform sampler2D mainTexture;
+layout(binding = 1) uniform sampler2D brightTexture;
+
+
+void main()
+{
+    fragColor = texture(brightTexture, uvCoord);
+}
