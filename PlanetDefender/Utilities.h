121b23849006617434a51098ae5fb11d63605a8e
andrewyung <andrewyung@uvic.ca>
2020-02-29 22:49:39 -0500

    Function for retrieving rotaton angle and axis from matrix

--- PlanetDefender/Utilities.h
+++ PlanetDefender/Utilities.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <utility>
+
 #include "WindowCanvas.h"
 
 glm::vec3 mapToHemisphere(float e, float x, float y) {
@@ -10,4 +12,17 @@ glm::vec3 mapToHemisphere(float e, float x, float y) {
 	float pv = sin_theta * sin_phi;
 	float pw = cos_theta;
 	return { pu, pv, pw };
+}
+
+std::pair<float, glm::vec3> getRotationAxisAngle(glm::mat4 r)
+{
+	float angle = glm::acos((r[0][0] + r[1][1] + r[2][2] - 1) / 2);
+
+	glm::vec3 axis{
+		r[2][1] - r[1][2] / sqrt(glm::exp2(r[2][1] - r[1][2]) + glm::exp2(r[0][2] - r[2][0]) + glm::exp2(r[1][0] - r[0][1])),
+		r[0][2] - r[2][0] / sqrt(glm::exp2(r[2][1] - r[1][2]) + glm::exp2(r[0][2] - r[2][0]) + glm::exp2(r[1][0] - r[0][1])),
+		r[1][0] - r[0][1] / sqrt(glm::exp2(r[2][1] - r[1][2]) + glm::exp2(r[0][2] - r[2][0]) + glm::exp2(r[1][0] - r[0][1])),
+	};
+
+	return std::make_pair(angle, axis);
 }
